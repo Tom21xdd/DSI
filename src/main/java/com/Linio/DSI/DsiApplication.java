1994c7941afb322f861cec975c4224871a229ad0
Tom21xdd <johan05182002@hotmail.com>
2022-09-22 17:28:53 -0500

    primer commit

---
+++ src/main/java/com/Linio/DSI/DsiApplication.java
@@ -0,0 +1,13 @@
+package com.Linio.DSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Linio/DSI/DsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Linio.DSI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
