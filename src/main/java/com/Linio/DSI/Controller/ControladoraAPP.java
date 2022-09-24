ab23192556a66f16f637b1fbb3975fdd225caed7
Andres Felipe <110916040+andres003@users.noreply.github.com>
2022-09-24 18:00:49 -0500

    Add files via upload

---
+++ src/main/java/com/Linio/DSI/Controller/ControladoraAPP.java
@@ -0,0 +1,31 @@
+package com.Linio.DSI.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class ControladoraAPP {
+
+	@GetMapping({ "/", "/login" })
+	public String index() {
+
+		return "index";
+	}
+
+	@GetMapping("/menu")
+	public String hello() {
+
+		return "menu";
+	}
+
+	@GetMapping("/user")
+	public String user() {
+
+		return "user";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+
+		return "admin";
+	}
+
+}
