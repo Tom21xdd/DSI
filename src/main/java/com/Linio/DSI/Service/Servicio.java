d5d8f40a0b2434c48fa7ce5db1383d6308fed1ce
SneiderQuintero <dasquirodaron@gmail.com>
2022-09-25 01:38:30 -0500

    Se sobrecargaron las funciones del CRUD mediante el repositorio

---
+++ src/main/java/com/Linio/DSI/Service/Servicio.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Linio.DSI.Service;
+import com.Linio.DSI.Entidad.Persona;
+import com.Linio.DSI.repositorio.Repositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author SneiderQuintero
+ */
+@Service
+public class Servicio implements ServicioInterfaz{
+    
+    @Autowired
+    private Repositorio repositorio;
+    
+    @Override
+    public Persona crear(Persona persona){
+        return repositorio.save(persona);
+    }
+    @Override
+    public Persona buscar(Long documento){
+        return repositorio.findById(documento).get();
+        
+    }
+    @Override
+    public Persona actualizar(Persona persona){
+        return repositorio.save(persona);
+        
+    }
+    
+    @Override
+    public void eliminar(Long documento){
+        repositorio.deleteById(documento);
+    }
+}
