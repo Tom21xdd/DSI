7ebe716764629162a3b4384b424cd212d114f255
SneiderQuintero <dasquirodaron@gmail.com>
2022-09-25 01:27:10 -0500

    Se especificaron las funciones del CRUD

---
+++ src/main/java/com/Linio/DSI/Service/ServicioInterfaz.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Linio.DSI.Service;
+import com.Linio.DSI.Entidad.Persona;
+/**
+ *
+ * @author SneiderQuintero
+ */
+public interface ServicioInterfaz {
+    public Persona crear(Persona persona);
+    public Persona buscar(Long documento);
+    public Persona actualizar(Persona persona);
+    public void eliminar(Long documento);
+}
